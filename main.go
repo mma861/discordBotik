85b4b36b84c01a6f461b386ee323a873e53d5930
mma861 <mironovmatvey4444@gmail.com>
2025-03-25 23:00:17 +0300

    import words from file

--- main.go
+++ main.go
@@ -1,12 +1,36 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
 )
+
+func readFile(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+func ifInStr(str string, arr []string) string {
+	for _, value := range arr {
+		if strings.Contains(str, value) {
+			return value
+		}
+	}
+	return "FNORD"
+}
 
 func main() {
 	token := os.Getenv("DISCORD_BOT_TOKEN")
@@ -17,8 +41,8 @@ func main() {
 		return
 	}
 
-	dg.AddHandler(messageCreate)
-
+	dg.AddHandler(messageReact)
+	dg.AddHandler(messageReply)
 	err = dg.Open()
 	if err != nil {
 		fmt.Println("Нет соединения:", err)
@@ -30,16 +54,29 @@ func main() {
 	select {}
 }
 
-func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+func messageReact(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
-
+	pregnant, err := readFile("pregnant.txt")
+	if err != nil {
+		fmt.Println("Нет файла:", err)
+	}
 	content := strings.ToLower(m.Content)
-	if strings.Contains(content, "жоско") || strings.Contains(content, "жоска") {
+	// fmt.Println(strings.Contains(content, ifInStr(content, pregnant)))
+	// fmt.Println(ifInStr(content, pregnant))
+	if strings.Contains(content, ifInStr(content, pregnant)) {
 		err := s.MessageReactionAdd(m.ChannelID, m.ID, "🫃")
 		if err != nil {
-			fmt.Println("Нет реакции:", err)
+			fmt.Println("Нет реакции:", err, "🫃")
 		}
+	}
+}
+func messageReply(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	if strings.Contains(m.Content, "🫃") {
+		s.ChannelMessageSendReply(m.ChannelID, "🫃", m.Reference())
 	}
 }
